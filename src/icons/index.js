16b87315d09975b05f957aa89290431825958c99
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-09-10 09:59:11 +0200

    added snow

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,11 @@
 import Clouds from "./cloud.png";
 import Rain from "./rain.png";
+import Snow from "./rain.png";
 import Sun from "./sun.png";
 
 export default {
     Clouds,
     Sun,
-    Rain
+    Rain,
+    Snow
   }
