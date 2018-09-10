faea616ef1b0e965cb8029fc538893457450e163
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-09-10 20:55:05 +0200

    updated api call with async and await

--- src/Weather.js
+++ src/Weather.js
@@ -8,26 +8,29 @@ const WeatherCard = styled.div`
   display: flex;
 `
 
+async function setWeatherToState(url) {
+  const response = await axios.get(url)
+  const wantedObj = response.data.list.filter((time) => time.dt_txt.endsWith('18:00:00'));
+  const wantedTemps = wantedObj.map((obj) => (
+    obj.main.temp
+  ));
+  const weatherStatus = wantedObj.map((obj) => (
+    obj.weather[0].main
+  ));
+  this.setState({ temps: wantedTemps, loading: false, status: weatherStatus });
+}
+
 class Weather extends React.Component {
   constructor(props) {
     super(props);
     this.state = { temps: [], loading: true, status: [] }
+    this.setWeather = setWeatherToState.bind(this);
   }
-  componentDidMount() {
-    axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=Lund,se&units=metric&APPID=aef11d8026de35e63a97cc8412ef1b2f`)
-      .then(res => {
-        const wantedObj = res.data.list
-          .filter((time) => time.dt_txt.endsWith('18:00:00'));
-        const wantedTemps = wantedObj.map((obj) => (
-          obj.main.temp
-        ));
-        const weatherStatus = wantedObj.map((obj) => (
-          obj.weather[0].main
-        ));
-        this.setState({ temps: wantedTemps, loading: false, status: weatherStatus });
-      });
 
+  async componentDidMount() {
+    await this.setWeather(`http://api.openweathermap.org/data/2.5/forecast?q=Lund,se&units=metric&APPID=aef11d8026de35e63a97cc8412ef1b2f`);
   }
+
   render() {
     const numDays = 5;
     var days = [];
